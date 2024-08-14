437ea6932f87a9aedcaa6531f8c94984d5b3575e
Anis Afifi <anis@afifi.pro>
2024-08-14 18:41:33 +0600

    214

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,37 @@
+import { CheckIcon } from "@radix-ui/react-icons";
+import { GiSpiderWeb } from "react-icons/gi"; 
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
-export default function Home() {
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Link from "next/link";
+
+const notifications = [
+  {
+    title: "Your call has been confirmed.",
+    description: "1 hour ago",
+  },
+  {
+    title: "You have a new message!",
+    description: "1 hour ago",
+  },
+  {
+    title: "Your subscription is expiring soon!",
+    description: "2 hours ago",
+  },
+];
+
+type CardProps = React.ComponentProps<typeof Card>;
+
+export default function Home({ className, ...props }: CardProps) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-6">
       
