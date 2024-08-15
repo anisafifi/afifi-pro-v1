6f01bd250285093ff0fbb938cdbfa190db0e5711
Anis Afifi <anis@afifi.pro>
2024-08-15 13:58:03 +0600

    20240815

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,14 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Afifi",
-  description: "The family tree of greatest family",
+  title: "Afifi - The family tree of ",
+  description:
+    "Afifi - The family tree of this ancient and noble lineage traces its roots through generations of distinguished figures, each contributing to the rich history and cultural heritage of their ancestry.",
 };
 
 export default function RootLayout({
@@ -18,9 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn(
-          "min-h-screen bg-background font-sans antialiased",
-        )}>{children}</body>
+      <body className={cn("min-h-screen bg-background font-sans antialiased")}>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,30 +11,49 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen justify-center items-center p-4">
-      <Card className="w-auto">
-        <CardHeader>
-          <Avatar>
+    <main className="flex min-h-screen justify-center items-center">
+      <Card className="w-auto max-w-[340px] p-4">
+        <CardHeader className="flex justify-center items-center">
+
+          <Avatar
+          className=""
+          >
             <AvatarImage
               src="https://marketing.afifi.pro/asset/1:avater"
               alt="Anis Afifi"
             />
             <AvatarFallback>Anis Afifi</AvatarFallback>
           </Avatar>
-          <CardTitle className="flex justify-center items-center">
+
+          <CardTitle>
             Anis Afifi
+
           </CardTitle>
-          <CardDescription className="flex justify-center items-center">
-            This is a test. The website coming soon...
+
+          <CardDescription
+          className="text-justify"
+          >
+          Entrepreneur and businessman with a big dream to make the world a better place for humankind.
           </CardDescription>
+      
         </CardHeader>
-        <CardContent></CardContent>
+        <hr className="my-4" />
+        <CardContent>
+        <Link
+          href="https://anisafifi.com"
+          >  
+          <Button 
+          variant="outline"
+          className="text-end"
+          >Go to main site</Button>
+          </Link>
+        </CardContent>
         <CardFooter className="flex justify-between">
-          <Button variant="outline">Cancel</Button>
-          <Button>Deploy</Button>
+          
         </CardFooter>
       </Card>
     </main>
