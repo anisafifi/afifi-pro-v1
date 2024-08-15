a31553b841e31d058b0b59e750e8f6a7315e31e4
Anis Afifi <anis@afifi.pro>
2024-08-15 11:43:38 +0600

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-import { CheckIcon } from "@radix-ui/react-icons";
-import { GiSpiderWeb } from "react-icons/gi"; 
-import { cn } from "@/lib/utils";
+import * as React from "react";
+
 import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 import {
   Card,
   CardContent,
@@ -11,30 +12,31 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import Link from "next/link";
-
-const notifications = [
-  {
-    title: "Your call has been confirmed.",
-    description: "1 hour ago",
-  },
-  {
-    title: "You have a new message!",
-    description: "1 hour ago",
-  },
-  {
-    title: "Your subscription is expiring soon!",
-    description: "2 hours ago",
-  },
-];
-
-type CardProps = React.ComponentProps<typeof Card>;
-
-export default function Home({ className, ...props }: CardProps) {
+export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-6">
-      
+    <main className="flex min-h-screen justify-center items-center p-4">
+      <Card className="w-auto">
+        <CardHeader>
+          <Avatar>
+            <AvatarImage
+              src="https://marketing.afifi.pro/asset/1:avater"
+              alt="Anis Afifi"
+            />
+            <AvatarFallback>Anis Afifi</AvatarFallback>
+          </Avatar>
+          <CardTitle className="flex justify-center items-center">
+            Anis Afifi
+          </CardTitle>
+          <CardDescription className="flex justify-center items-center">
+            This is a test. The website coming soon...
+          </CardDescription>
+        </CardHeader>
+        <CardContent></CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline">Cancel</Button>
+          <Button>Deploy</Button>
+        </CardFooter>
+      </Card>
     </main>
   );
 }
